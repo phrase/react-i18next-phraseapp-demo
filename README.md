1698005e597fd55b50d5e25db978f86dcb066461
docstun <manuel.boy@dynport.de>
2017-04-03 15:43:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# react-i18next-phraseapp Demo
+
+## Install
+
+    $ npm install
+
+## Run
+
+Start the server:
+
+    $ node app.js
+
+and then open the app at:
+
+    http://localhost:3003/
+
---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'))
+
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!')
+})
