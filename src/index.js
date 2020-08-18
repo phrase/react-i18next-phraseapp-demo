887aea7c9cf0291f53f487be145bc0a00f5b9331
Jakub Freisler <FRSgit@users.noreply.github.com>
2020-08-18 14:32:17 +0200

    chore: deprecate package (#17)
    package deprecation in favor of i18next-phrase-in-context-editor-post-processor
    add deprecation notice to README.md
    add deprecation console.log notification
    add deprecation notice to package.json description

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { PhraseAppProvider } from 'react-i18next-phraseapp';
 import MyApp from './MyApp';
 import i18n from './i18n';
 
+console?.log?.('This package has been deprecated. Consider migrating to i18next-phrase-in-context-editor-post-processor instead (https://www.npmjs.com/package/i18next-phrase-in-context-editor-post-processor).');
+
 ReactDOM.render(
   <I18nextProvider i18n={ i18n }>
     <PhraseAppProvider config={ window.PHRASEAPP_CONFIG }>
